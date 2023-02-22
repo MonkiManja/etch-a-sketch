2380f3ec1cfc390e86298c7d54b79f0f479fcc5a
MonkiManja <122291289+MonkiManja@users.noreply.github.com>
2023-02-22 01:24:48 -0300

    sidemenu and minor progress

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("canvas");
 
+let color ="#0000ff";
+
 let grid_num = 16;
 
 for(let i = 0; i < grid_num ** 2; i++){
@@ -7,10 +9,21 @@ for(let i = 0; i < grid_num ** 2; i++){
 }
 
 function createPixel(){
-    console.log("passd")
     let newItem = document.createElement("div");
     newItem.className = "testColor";
     canvas.appendChild(newItem);
 }
 
+const canvasNodeList = document.querySelectorAll("#canvas > .testColor")
 
+
+function paint(pixel, color){
+    console.log("painted!")
+    pixel.style.backgroundColor = color;
+}
+
+console.log(canvasNodeList)
+canvasNodeList.forEach((pixel) => {
+    pixel.addEventListener("click", paint(pixel, color))
+});
+// Now the grid is done. Now i have to think how to draw in it
